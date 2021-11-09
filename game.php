22dcd2cec569aacebd62da005c09aabe20e0f16a
arnabmitra410 <93967681+arnabmitra410@users.noreply.github.com>
2021-11-09 12:59:48 +0530

    Update game.php

--- game.php
+++ game.php
@@ -45,7 +45,7 @@ $result = check($computer, $human);
 <!DOCTYPE html>
 <html>
 <head>
-<title>Anamitra Musib 32ce3337</title>
+<title>Arnab Mitra cef020f8</title>
 <?php require_once "bootstrap.php"; ?>
 </head>
 <body>
